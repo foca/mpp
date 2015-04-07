e3aaf64b68bf553913749dd7318368798e6ecd8b
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2015-04-07 18:17:25 -0300

    Nicer -v and -h output

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	flag "github.com/ogier/pflag"
+	"io"
 	"os"
 )
 
@@ -21,11 +22,24 @@ func main() {
 		"Add paths to search for templates")
 	showVersion := flag.BoolP("version", "v", false,
 		"Print the version of "+os.Args[0])
+	showHelp := flag.BoolP("help", "h", false,
+		"Print this help message")
+
 	flag.Parse()
 
 	if *showVersion {
-		fmt.Println(VERSION)
+		fmt.Println(os.Args[0], "version", VERSION)
 		os.Exit(0)
+	}
+
+	if *showHelp {
+		Usage(os.Stdout)
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) == 0 {
+		Usage(os.Stderr)
+		os.Exit(1)
 	}
 
 	err := AddTemplatePaths(*loadPaths)
@@ -50,3 +64,20 @@ func assertNilErr(err error) {
 		os.Exit(1)
 	}
 }
+
+func Usage(out io.Writer) {
+	fmt.Fprintf(out, HELP, os.Args[0], os.Args[0])
+}
+
+const HELP = `Usage: %s [options] files...
+
+    mpp is a mini preprocessor.
+
+Options:
+
+    -I, --include=[<dir>:<dir>...]: Include dirs in the template search path.
+    -M, --make:                     Print Make-style dependencies.
+    -v, --version:                  Print the current version of %s.
+    -h, --help:                     Print this help text.
+
+`
